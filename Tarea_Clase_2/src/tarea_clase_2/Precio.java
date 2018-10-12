4753db6fa2d8c127a6ce0831f6e65a1539da4cc1
Franz Paccha <f-a-p-c-1999@hotmail.com>
2018-10-12 10:24:52 -0500

    subiendo

---
+++ Tarea_Clase_2/src/tarea_clase_2/Precio.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_clase_2;
+
+/**
+ *
+ * @author user
+ */
+public class Precio {
+    double euros;
+    public double obtener(){
+        
+    }
+    public void agregar(double x){
+        
+    }
+}
